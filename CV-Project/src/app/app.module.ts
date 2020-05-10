11576a93bc3b8e385a2400df8db874d23fe89530
Theodoros Konstantoulias <thodoriskonstantoulias@yahoo.com>
2020-05-10 11:57:22 +0300

    Navbar draft

--- CV-Project/src/app/app.module.ts
+++ CV-Project/src/app/app.module.ts
@@ -4,12 +4,14 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule
