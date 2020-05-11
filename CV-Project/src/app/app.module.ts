d6ebbdaa941cd22f39968fd999296d176d59868c
Theodoros Konstantoulias <thodoriskonstantoulias@yahoo.com>
2020-05-11 20:21:47 +0300

    Setting up the main components

--- CV-Project/src/app/app.module.ts
+++ CV-Project/src/app/app.module.ts
@@ -1,20 +1,34 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { AppRoutingModule } from './app.routing.module';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import { HomeComponent } from './home/home.component';
+import { ExprerienceComponent } from './exprerience/exprerience.component';
+import { EducationComponent } from './education/education.component';
+import { SkillsComponent } from './skills/skills.component';
+import { InfoComponent } from './info/info.component';
+import { ContactComponent } from './contact/contact.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     FooterComponent,
-    NavbarComponent
+    NavbarComponent,
+    HomeComponent,
+    ExprerienceComponent,
+    EducationComponent,
+    SkillsComponent,
+    InfoComponent,
+    ContactComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule 
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ CV-Project/src/app/exprerience/exprerience.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-exprerience',
+  templateUrl: './exprerience.component.html',
+  styleUrls: ['./exprerience.component.css']
+})
+export class ExprerienceComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
